496cd07a107739fd304028b90224decce00787a0
RyunosukeKurokawa <kuroryu.basket@gmail.com>
2023-05-17 18:41:51 +0900

    Initial Commit

---
+++ Count/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Count
+//
+//  Created by 黒川龍之介 on 2023/05/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Count/CountApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountApp.swift
+//  Count
+//
+//  Created by 黒川龍之介 on 2023/05/17.
+//
+
+import SwiftUI
+
+@main
+struct CountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
