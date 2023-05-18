578542235248477a4f9227bcfca02481113a603e
RyunosukeKurokawa <kuroryu.basket@gmail.com>
2023-05-19 00:30:13 +0900

    commit

--- Count/ContentView.swift
+++ Count/ContentView.swift
@@ -1,22 +1,51 @@
-//
-//  ContentView.swift
-//  Count
-//
-//  Created by 黒川龍之介 on 2023/05/17.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var textArray: [TextModel] = []
+    let targetText = "イラッ"  // 表示したい文字列
+    let X = UIScreen.main.bounds.width / 2
+    let Y = UIScreen.main.bounds.height
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            ForEach(textArray, id: \.id) { textModel in
+                Text(textModel.text)
+                    .font(.largeTitle)
+                    .position(textModel.position)
+            }
+            
+            Button(action: {
+                let randomPosition = self.randomPosition()
+                let randomTextModel = TextModel(text: targetText, position: randomPosition)
+                textArray.append(randomTextModel)
+            }) {
+                Text("イラ")
+                    .bold()
+                    .padding()
+                    .frame(width: 500, height: 500)
+                    .foregroundColor(Color.black)
+                    .background(Color.red)
+                    .clipShape(Circle())
+            }
+            .position(x:X,y:Y)
         }
-        .padding()
     }
+    
+    func randomPosition() -> CGPoint {
+        let maxX = UIScreen.main.bounds.width
+        let maxY = UIScreen.main.bounds.height - 200
+        
+        let randomX = CGFloat.random(in: 0...maxX)
+        let randomY = CGFloat.random(in: 0...maxY)
+        
+        return CGPoint(x: randomX, y: randomY)
+    }
+}
+
+struct TextModel: Identifiable {
+    let id = UUID()
+    let text: String
+    let position: CGPoint
 }
 
 struct ContentView_Previews: PreviewProvider {
